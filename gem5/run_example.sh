37f5e6a2d6a5511cafabd1f43e02a52fe9546556
aserenate <48470844+aserenate@users.noreply.github.com>
2021-01-21 16:28:19 +0800

    Update run_example.sh
    create initial output_data txt

--- gem5/run_example.sh
+++ gem5/run_example.sh
@@ -1,7 +1,9 @@
-# generate task lists
+# generate task listsstardard_create_file.py
 python3 ../standard_cpu_task/rs_dataflow.py 128
+python3 ../standard_cpu_output/stardard_create_file.py
 
 cp ../standard_cpu_task/*.txt ../cpu_task/
+cp ../standard_cpu_output/*.txt ../cpu_output/
 ./build/Garnet_standalone/gem5.opt \
 	configs/example/garnet_synth_traffic.py \
 	--topology=Mesh_XY \
